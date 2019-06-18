fbab1b5ab368d27bc86518add962a7c7ff3d7025
yongliang.li <yongliang.li@sigmastar.com.cn>
2019-06-18 10:58:48 +0800

    add Miscellaneous

---
+++ minigui_dev_kit/gui/config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MODULE=$1
+PREFIX=$2
+EPREFIX=$3
+SDKLIB=$4
+SDKINC=$5
+UAPIINC=$6
+MINIGUI_CROSS_COMPILE=$7
+PLAT_DEPENDENT_LIB=$8
+GRAPHICACCEL=$9
+cd ${MODULE}
+if [ -e "configure" ]; then
+touch ./configure
+fi
+if [ -e "conf.sh" ]; then
+export PLATDEPENDENTLIB=${PLAT_DEPENDENT_LIB}
+sh conf.sh ${PREFIX} ${EPREFIX} ${SDKLIB} ${SDKINC} ${UAPIINC} ${MINIGUI_CROSS_COMPILE} ${GRAPHICACCEL}
+fi
+pwd
